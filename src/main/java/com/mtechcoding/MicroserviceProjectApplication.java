60e34df46a81d3ac28425bc0084464b90982ad11
Mithilesh Kumar <35111020+mithileshkumar11@users.noreply.github.com>
2025-11-02 14:43:29 +0530

    Initial microservice project commit

---
+++ src/main/java/com/mtechcoding/MicroserviceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mtechcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProjectApplication.class, args);
+	}
+
+}
